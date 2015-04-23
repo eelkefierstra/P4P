c00b04e17dfcb6f9ad5b040bd769acbffb33e666
Dudecake <coen_koomen@hotmail.com>
2015-04-23 08:58:04 +0200

    more stuff that doesn't work

--- Lepra/src/ServoController.java
+++ Lepra/src/ServoController.java
@@ -1,41 +1,56 @@
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 //import org.opencv.core.Point;
 
+
+
+import jdk.dio.DeviceManager;
+import jdk.dio.gpio.GPIOPin;
+import jdk.dio.pwm.*;
+/*
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinPwmOutput;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
-
+*/
 public class ServoController implements Runnable
 {
-	
+	private GPIOPin pin; 
 	private boolean running;
 	private int i;
 	private int[] servos = { 0, 0, 0, 0 };
 	
 	// throws some random exception, wut?
 	// create gpio controller
-    private final GpioController gpio = GpioFactory.getInstance();
+    //private final GpioController gpio = GpioFactory.getInstance();
     
     // provision gpio pin #01 as an output pin and turn on
     //final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
-    private final GpioPinPwmOutput[] pins = { gpio.provisionPwmOutputPin(RaspiPin.GPIO_01) };
+    //private final GpioPinPwmOutput[] pins = { gpio.provisionPwmOutputPin(RaspiPin.GPIO_01) };
 	
 	
 	public ServoController()
 	{
 		running = true;
 		i = 0;
+		try
+		{
+			pin = DeviceManager.open(7);
+		}
+		catch(Exception e)
+		{
+			Logger.getLogger(ServoController.class.getName()).log(Level.SEVERE, null, e);
+		}
         // set shutdown state for this pin
         //pin.setShutdownOptions(true, PinState.LOW);
-		
+		/*
 		for(int i = 0; i < pins.length; i++)
 		{
 			pins[i].setShutdownOptions(true, PinState.LOW);
 		}
-		
+		*/
 	}
 	
 	public void setPWM(int i)
@@ -50,11 +65,13 @@ public class ServoController implements Runnable
 			while(running)
 			{
 				System.out.println("Foo " + i);
-				pins[0].setPwm(i);
+				//pins[0].setPwm(i);
+				pin.setValue(true);
 				Thread.sleep(500);
+				pin.setValue(false);
 			}
 		}
-		catch(InterruptedException e)
+		catch(Exception e)
 		{
 			Logger.getLogger(ServoController.class.getName()).log(Level.SEVERE, null, e);
 		}
@@ -71,12 +88,12 @@ public class ServoController implements Runnable
 	*/
 	private void setServos()
 	{
-		
+		/*
 		for(byte i = 0; i < pins.length; i++)
 		{
 			pins[i].setPwm(servos[i]);
 		}
-		
+		*/
 	}
 	
 	public void dinges()
@@ -84,19 +101,13 @@ public class ServoController implements Runnable
 		running = false;
         try
         {
-        	gpio.shutdown();
-            //pin.close();
+        	//gpio.shutdown();
+            pin.close();
         }
-        finally
-        {
-        	
-        }
-        /*
         catch (IOException ex)
         {
-            Logger.getLogger(MeerStuff.class.getName()).
-                log(Level.SEVERE, null, ex);
-        }*/
+            Logger.getLogger(ServoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
 	}
 }
