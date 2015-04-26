07622c58fb8863fc9e85c276edc9de2d18f07371
Dudecake <coen_koomen@hotmail.com>
2015-04-26 21:20:40 +0200

    Goodbye jme, I won't miss you.

--- Meer Tester/src/Main.java
+++ Meer Tester/src/Main.java
@@ -1,33 +1,33 @@
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class GLADos
+public class Main
 {
 	// TODO Make MeerStuff a List
 	private ServoController t;
 	//private GUI gui = new GUI();
 	
-	public GLADos()
-	{
-		
+    public static void main(String[] args)
+    {
+		Main p = new Main();
 		// TODO Auto-generated constructor stub
 		//System.out.println("Foo");
-		t = new ServoController();
-		new Thread(t).start();
+		p.t = new ServoController();
+		new Thread(p.t).start();
 		
 		try
 		{
 			for(int i = 0; i < Integer.MAX_VALUE; i++)
 			{
-				t.setPWM(i);
+				p.t.setPWM(i);
 				Thread.sleep(50);
 			}
 		} 
 		catch (InterruptedException e) 
 		{
 			// TODO Auto-generated catch block
-			Logger.getLogger(GLADos.class.getName()).log(Level.SEVERE, null, e);
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
 		}
-		t.dinges();
+		p.t.dinges();
 	}
 }
