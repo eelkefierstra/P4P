25cdefa227760c4f027c2cd54b79cdd5cb9f277a
Dudecake <coen_koomen@hotmail.com>
2015-05-11 15:56:50 +0200

    meer servostuff

--- Meer Tester/src/Main.java
+++ Meer Tester/src/Main.java
@@ -19,14 +19,14 @@ public class Main
 			float i = 0.025f;
 			for(;;)
 			{
-				for(; i <= 0.01f; i += 0.005f)
+				for(; i <= 0.01f; i += 0.00125f)
 				{
 					ServoController.WritePWM(23, i);
 					//p.gui.label.setText("PWM = " + i);
 					//p.gui.button.setText(p.gui.);
 					Thread.sleep(50);
 				}
-				for(; i >= 0.01f; i -= 0.005f)
+				for(; i >= 0.01f; i -= 0.00125f)
 				{
 					ServoController.WritePWM(23, i);
 					//p.gui.label.setText("PWM = " + i);
