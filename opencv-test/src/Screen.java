d80c9616c0b66c74a40ccefb0bc58c91c2ee7853
Dudecake <coen_koomen@hotmail.com>
2015-05-13 21:08:00 +0200

    now with 100% more visible gui!

--- opencv-test/src/Screen.java
+++ opencv-test/src/Screen.java
@@ -8,10 +8,14 @@ public class Screen extends JFrame
 	 * 
 	 */
 	private static final long serialVersionUID = -5826058727264357858L;
-	public JLabel screen = new JLabel();
+	public JLabel screen = new JLabel("hvuibvisbvcisvbisvbh");
 	
 	public Screen()
 	{
-		
+		this.add(screen);
+		this.setSize(1280, 720);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
 	}
 }
