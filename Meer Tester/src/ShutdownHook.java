622d50dfd27240bbdb3d06e1c05667ff888fffe6
Dudecake <coen_koomen@hotmail.com>
2015-06-17 14:10:28 +0200

    server werkt

--- Meer Tester/src/ShutdownHook.java
+++ Meer Tester/src/ShutdownHook.java
@@ -1,6 +1,9 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class ShutdownHook 
 {
-	public void attachShutDownHook()
+	public void attachShutDownHook(ServerSocket server)
 	{
 		Runtime.getRuntime().addShutdownHook(new Thread()
 		{
@@ -8,6 +11,15 @@ public class ShutdownHook
 			public void run()
 			{
 				//ServoController.ResetPWM();
+				try
+				{
+					System.out.println("Closing server");
+					server.close();
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
 				System.out.println("exit");
 			}
 		});
